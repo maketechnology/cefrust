12812fdc32e52c2ce8cf8f65f229759063de48c6
Guillermo Zunino <guillez@gmail.com>
2017-01-03 15:00:38 -0300

    cef args seems not to work

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ use std::ffi;
 use std::os::raw;
 
 fn main() {
-    let argv:Vec<ffi::CString> = std::env::args_os().map(|arg| { ffi::CString::new(arg.into_string().unwrap()).unwrap() } ).collect();
-    let args:Vec<*const ::std::os::raw::c_char> = argv.into_iter().map(|arg| { arg.as_ptr() } ).collect();
+    let argv:Vec<ffi::CString> = std::env::args_os().map(|arg| {
+        //println!("argv: {:?}", arg);     
+        ffi::CString::new(arg.into_string().unwrap()).unwrap() 
+    } ).collect();
+    let args:Vec<*const ::std::os::raw::c_char> = argv.into_iter().map(|arg| { 
+        println!("args: {:?}", arg);
+        arg.as_ptr() 
+    } ).collect();
     //fuse_main_real(args.len() as c_int, args.as_ptr() as *const *const c_char, .... );
  
     // Structure for passing command-line arguments.
@@ -15,10 +21,16 @@ fn main() {
         argc : args.len() as std::os::raw::c_int,
         argv : args.as_ptr() as *mut *mut std::os::raw::c_char
     };
-    println!("Hello CEF: {}", main_args.argc);
+    println!("Hello CEF, ARGS: {}", main_args.argc);
+
+    //println!("arg0: {}", argv[0].into_string().unwrap());
+    //println!("arg0: {}", args[0]);
+
+    //std::process::exit(0);
 
     // Execute the sub-process logic, if any. This will either return immediately for the browser
     // process or block until the sub-process should exit
+    println!("Calling cef_execute_process");
     let exit_code:raw::c_int = unsafe{
         let exit_code = cef::cef_execute_process(&main_args, std::ptr::null_mut(), std::ptr::null_mut());
         exit_code
@@ -30,13 +42,23 @@ fn main() {
         std::process::exit(exit_code);
     }
 
-    let empty_str = cef::cef_string_t {
+    let mut empty_str = cef::cef_string_t {
         str: std::ptr::null_mut(), 
         length: 0, 
         dtor: std::option::Option::None
     };
     //cef::cef_string_utf16_set("", 0, empty_str, true);
-    cef::cef_string_utf8_to_utf16("".as_ptr() as *mut std::os::raw::c_char, 0, &mut empty_str);
+    unsafe {
+        cef::cef_string_utf8_to_utf16("".as_ptr() as *mut std::os::raw::c_char, 0, &mut empty_str);
+    }
+
+    let mut locales_cef = cef::cef_string_t {str: std::ptr::null_mut(), length: 0, dtor: std::option::Option::None};
+    let locales = "/home/gzunino/workspaces/rust/cefrust/target/debug/locales";
+    unsafe {cef::cef_string_utf8_to_utf16(locales.as_ptr() as *mut std::os::raw::c_char, locales.len(), &mut locales_cef);}
+
+    let mut resources_cef = cef::cef_string_t {str: std::ptr::null_mut(), length: 0, dtor: std::option::Option::None};
+    let resources = "/home/gzunino/workspaces/rust/cefrust/target/debug/Resources";
+    unsafe {cef::cef_string_utf8_to_utf16(resources.as_ptr() as *mut std::os::raw::c_char, resources.len(), &mut resources_cef);}
 
     let settings = cef::_cef_settings_t {
         size: 344usize,
@@ -53,11 +75,11 @@ fn main() {
         persist_user_preferences: 1,
         user_agent: empty_str,
         product_version: empty_str,
-        locale: empty_str,
+        locale: locales_cef,
         log_file: empty_str,
         log_severity: cef::_bindgen_ty_3::LOGSEVERITY_VERBOSE,
         javascript_flags: empty_str,
-        resources_dir_path: empty_str,
+        resources_dir_path: resources_cef,
         locales_dir_path: empty_str,
         pack_loading_disabled: 0,
         remote_debugging_port: 0,
@@ -72,11 +94,16 @@ fn main() {
     // Initialize CEF in the main process.
     let app = std::ptr::null_mut();
 
-    cef::cef_initialize(&main_args, &settings, app, std::ptr::null_mut());
+    unsafe {
+        println!("Calling cef_initialize");
+        cef::cef_initialize(&main_args, &settings, app, std::ptr::null_mut());
+        
+        println!("Calling cef_run_message_loop");
+        // Run the CEF message loop. This will block until CefQuitMessageLoop() is called.
+        cef::cef_run_message_loop();
 
-    // Run the CEF message loop. This will block until CefQuitMessageLoop() is called.
-    cef::cef_run_message_loop();
-
-    // Shut down CEF.
-    cef::cef_shutdown();
+        println!("Calling cef_shutdown");
+        // Shut down CEF.
+        cef::cef_shutdown();
+    }
 }
